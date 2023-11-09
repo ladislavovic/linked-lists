c3ecda8cdf3c3781be7379d5c7f28ce8012573e6
ladislav.kulhanek <ladislav.kulhanek@cross-ni.com>
2023-11-09 12:11:47 +0100

    another

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# linked-lists
+# Sorted Linked Lists
+
+This library provides implementation of sorted linked list. There are two implementations:
+* `IntegerSortedLinkedList` - for `Integer` items
+* `StringSortedLinkedList` - for `String` items
+
+## Basic Features
+Call constructor to create an instance of the list:
+```java
+
+// Create an empty list
+var list = new IntegerSortedLinkedList();
+
+// Create a list containing the given items
+var list = new IntegerSortedLinkedList(List.of(10, 5, 12));
+```
+
+Collections implements (almost all) methods from `java.util.List`
+interface, so you can use it like "standard" Java lists:
+
+```java
+var list = new StringSortedLinkedList();
+
+// add a few items
+list.add("foo");
+list.add("bar");
+list.add("baz");
+
+// remove an item by index
+list.remove(0);
+
+// remove an item by value
+list.remove("foo");
+
+// create an Array
+String[] arr = list.toArray(new String[0]);
+```
+
+The collections can contain `null` items:
+```java
+var list = new StringSortedLinkedList();
+list.add(null);
+list.add(null);
+assertEquals(2, list.size());
+```
+
+## Sorting
+The lists are sorted - every time you add an item to the list, it
+is put in the right place according to sorting.
+
+The default sorting sorts the items in natural way and `null` items are
+sorted to the beginning.
+
+If you need a different sorting, provide a `Comparator` instance during
+the list creation, then the collection is sorted according to you 
+comparator:
+```java
+Comparator<String> customComparator = ...;
+var list = new StringSortedLinkedList(customComparator);
+```
+
+## Pointers
+
+## Concurrency
+The implementation is not thread safe. If you need to access the list
+from more threads, you must synchronize them. Otherwise the collection
+can be in inconsisted state.
+
+## Missing features
+* method `List.subList(int fromIndex, int toIndex)` is not implemented
+* method `List.retainAll(Collection<?> c)` is not implemented
--- src/main/java/cz/kul/linkedlists/BaseSortedLinkedList.java
+++ src/main/java/cz/kul/linkedlists/BaseSortedLinkedList.java
@@ -9,11 +9,20 @@ import java.util.Objects;
 
 class BaseSortedLinkedList<T> implements List<T> {
 
+	/**
+	 * The first node of the linked linst
+	 */
 	private Node<T> first;
 
+	/**
+	 * The current size of the list
+	 */
 	private int size = 0;
 
-	private Comparator<T> comparator;
+	/**
+	 * The comparator for value sorting
+	 */
+	private final Comparator<T> comparator;
 
 	BaseSortedLinkedList(Comparator<T> comparator) {
 		this.comparator = comparator;
@@ -21,11 +30,10 @@ class BaseSortedLinkedList<T> implements List<T> {
 
 	BaseSortedLinkedList(Collection<T> items, Comparator<T> comparator) {
 		this.comparator = comparator;
-		for (T item : items) {
-			add(item); // TODO replace with addAll
-		}
+		addAll(items);
 	}
 
+	@Override
 	public boolean add(T val) {
 		Node<T> item = createNode(val);
 
@@ -55,6 +63,239 @@ class BaseSortedLinkedList<T> implements List<T> {
 		return true;
 	}
 
+	@Override
+	public boolean remove(Object o) {
+		Node<T> node = firstNodeAndIndex(o).node;
+		if (node != null) {
+			removeNode(node);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		for (Object item : c) {
+			if (!contains(item)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		for (T val : c) {
+			add(val);
+		}
+		return !c.isEmpty();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends T> c) {
+		throw new UnsupportedOperationException("Adding to particular index is not supported in sorted list.");
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		boolean modified = false;
+		for (Object val : c) {
+			if (remove(val)) {
+				modified = true;
+			}
+		}
+		return modified;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public T remove(int index) {
+		checkIndex(index);
+		Node<T> toRemove = getNode(index);
+		removeNode(toRemove);
+		return toRemove.getValue();
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	@Override
+	public int indexOf(Object value) {
+		return firstNodeAndIndex(value).index;
+	}
+
+	@Override
+	public int lastIndexOf(Object value) {
+		int index = 0;
+		int lastMatch = -1;
+		for (Node<T> n = first; n != null; n = n.getNext()) {
+			if (Objects.equals(value, n.getValue())) {
+				lastMatch = index;
+			} else {
+				if (lastMatch >= 0) {
+					return lastMatch;
+				}
+			}
+			index++;
+		}
+		return lastMatch;
+	}
+
+	@Override
+	public boolean contains(Object value) {
+		return indexOf(value) >= 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return listIterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return toArray(new Object[0]);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <U> U[] toArray(U[] a) {
+
+		// TODO does it store ArrayStoreException when wrong array type?
+
+		if (a.length < size) {
+			a = (U[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+		}
+		Iterator<T> it = iterator();
+		int i = 0;
+		for (; it.hasNext(); i++) {
+			a[i] = (U) it.next();
+		}
+
+		if (a.length > size) {
+			a[size] = null;
+		}
+
+		return a;
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		return new BaseSortedLinkedListIterator();
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int index) {
+		// TODO impelement
+		return null;
+	}
+
+	@Override
+	public List<T> subList(int fromIndex, int toIndex) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void clear() {
+
+		// TODO GC comment
+		for (Node<T> n = first; n != null; ) {
+			Node<T> next = n.getNext();
+			n.setPrevious(null);
+			n.setNext(null);
+			n = next;
+		}
+		first = null;
+	}
+
+
+	@Override
+	public String toString() {
+		Iterator<T> it = iterator();
+		if (it.hasNext()) {
+			return "[]";
+		}
+
+		final int MAX = 10;
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < MAX && it.hasNext(); i++) {
+			sb.append(it.next());
+			if (it.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		if (size >= MAX) {
+			sb.append("...");
+		}
+
+		sb.append("]");
+		return sb.toString();
+	}
+
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof List)) {
+			return false;
+		}
+
+		Iterator<T> i1 = iterator();
+		Iterator<?> i2 = ((List<?>) o).iterator();
+		while (i1.hasNext() && i2.hasNext()) {
+			if (!Objects.equals(i1.next(), i2.next())) {
+				return false;
+			}
+		}
+		return i1.hasNext() == i2.hasNext();
+	}
+
+	@Override
+	public int hashCode() {
+		T firstVal = isEmpty() ? null : first.getValue();
+		return Objects.hash(firstVal, size);
+	}
+
+	public T get(int index) {
+		checkIndex(index);
+
+		Node<T> node = getNode(index);
+		return node.getValue();
+	}
+
+	@Override
+	public T set(int index, T element) {
+		return null;
+	}
+
+	@Override
+	public void add(int index, T element) {
+
+	}
+
+	private Node<T> createNode(T value) {
+		return new Node<T>(value);
+	}
+
+	private void checkIndex(int index) {
+		if (index >= size || index < 0) {
+			String msg = String.format("Index out of range. index=%s, size=%s", index, size);
+			throw new IndexOutOfBoundsException(msg);
+		}
+
+	}
+
 	private void insertBefore(Node<T> node, Node<T> inserted) {
 		inserted.setPrevious(node.getPrevious());
 		inserted.setNext(node);
@@ -81,49 +322,6 @@ class BaseSortedLinkedList<T> implements List<T> {
 		node.setNext(inserted);
 	}
 
-
-
-
-
-
-
-
-
-	@Override
-	public boolean remove(Object o) {
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		return false;
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		return false;
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends T> c) {
-		return false;
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		return false;
-	}
-
-
-
-
-
-
 	private Node<T> getNode(int index) {
 		Node<T> n = first;
 		for (long i = 0; i < index; i++) {
@@ -132,13 +330,10 @@ class BaseSortedLinkedList<T> implements List<T> {
 		return n;
 	}
 
-	public T remove(int index) {
-		checkIndex(index);
-
-		Node<T> toRemove = getNode(index);
-
+	private void removeNode(Node<T> toRemove) {
 		Node<T> previous = toRemove.getPrevious();
 		Node<T> next = toRemove.getNext();
+
 		if (previous != null) {
 			previous.setNext(next);
 		}
@@ -155,55 +350,25 @@ class BaseSortedLinkedList<T> implements List<T> {
 		}
 
 		size--;
-
-		return toRemove.getValue();
 	}
 
-	public int size() {
-		return size;
-	}
-
-	public boolean isEmpty() {
-		return first == null;
-	}
-
-	public int indexOf(Object value) {
+	private NodeAndIndex<T> firstNodeAndIndex(Object value) {
 		int index = 0;
 		for (Node<T> n = first; n != null; n = n.getNext()) {
 			if (Objects.equals(value, n.getValue())) {
-				return index;
+				return new NodeAndIndex<>(n, index);
 			}
 			index++;
 		}
-		return -1;
+		return new NodeAndIndex<>(null, -1);
 	}
 
-	public int lastIndexOf(Object value) {
-		int index = 0;
-		int lastMatch = -1;
-		for (Node<T> n = first; n != null; n = n.getNext()) {
-			if (Objects.equals(value, n.getValue())) {
-				lastMatch = index;
-			} else {
-				if (lastMatch >= 0) {
-					return lastMatch;
-				}
-			}
-			index++;
-		}
-		return lastMatch;
-	}
-
-	public boolean contains(Object value) {
-		return indexOf(value) >= 0;
-	}
-
-	private class SLLIterator implements ListIterator<T> {
+	private class BaseSortedLinkedListIterator implements ListIterator<T> {
 
 		Node<T> current;
 		int currentIndex = 0;
 
-		SLLIterator() {
+		BaseSortedLinkedListIterator() {
 			current = first;
 			currentIndex = 0;
 		}
@@ -262,135 +427,13 @@ class BaseSortedLinkedList<T> implements List<T> {
 
 	}
 
-	public Iterator<T> iterator() {
-		return listIterator();
-	}
+	private static class NodeAndIndex<T> {
+		Node<T> node;
+		int index;
 
-
-
-
-
-
-
-	@Override
-	public Object[] toArray() {
-		return new Object[0];
-	}
-
-	@Override
-	public <T1> T1[] toArray(T1[] a) {
-		return null;
-	}
-
-	public ListIterator<T> listIterator() {
-		return new SLLIterator();
-	}
-
-	@Override
-	public ListIterator<T> listIterator(int index) {
-		return null;
-	}
-
-	@Override
-	public List<T> subList(int fromIndex, int toIndex) {
-		return null;
-	}
-
-	public void clear() {
-
-		// TODO GC comment
-		for (Node<T> n = first; n != null; ) {
-			Node<T> next = n.getNext();
-			n.setPrevious(null);
-			n.setNext(null);
-			n = next;
-		}
-		first = null;
-	}
-
-
-	@Override
-	public String toString() {
-		Iterator<T> it = iterator();
-		if (it.hasNext()) {
-			return "[]";
-		}
-
-		final int MAX = 10;
-		StringBuilder sb = new StringBuilder();
-		sb.append("[");
-		for (int i = 0; i < MAX && it.hasNext(); i++) {
-			sb.append(it.next());
-			if (it.hasNext()) {
-				sb.append(", ");
-			}
-		}
-
-		if (size >= MAX) {
-			sb.append("...");
-		}
-
-		sb.append("]");
-		return sb.toString();
-	}
-
-	public boolean equals(Object o) {
-		if (o == this) {
-			return true;
-		}
-		if (!(o instanceof List)) {
-			return false;
-		}
-
-		Iterator<T> i1 = iterator();
-		Iterator<?> i2 = ((List<?>) o).iterator();
-		while(i1.hasNext() && i2.hasNext()) {
-			if (!Objects.equals(i1.next(), i2.next())) {
-				return false;
-			}
-		}
-		return i1.hasNext() == i2.hasNext();
-	}
-
-	@Override
-	public int hashCode() {
-		T firstVal = isEmpty() ? null : first.getValue();
-		return Objects.hash(firstVal, size);
-	}
-
-	public T get(int index) {
-		checkIndex(index);
-
-		Node<T> node = getNode(index);
-		return node.getValue();
-	}
-
-	@Override
-	public T set(int index, T element) {
-		return null;
-	}
-
-	@Override
-	public void add(int index, T element) {
-
-	}
-
-	public BaseSortedLinkedList<T> sublist(int from, int to) {
-		checkIndex(from);
-		checkIndex(to); // TODO to index can be equal to size
-
-		// TODO implement
-		return null;
-	}
-
-	private Node<T> createNode(T value) {
-		return new Node<T>(value);
-	}
-
-	private void checkIndex(int index) {
-		if (index >= size || index < 0) {
-			String msg = String.format("Index out of range. index=%s, size=%s", index, size);
-			throw new IndexOutOfBoundsException(msg);
+		public NodeAndIndex(Node<T> node, int index) {
+			this.node = node;
+			this.index = index;
 		}
 
 	}
